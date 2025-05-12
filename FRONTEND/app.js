9dad56b683dca151d979722c5de6274278ef2d71
Bernadetta Fabianová <florianfabian53@gmail.com>
2025-05-12 21:32:49 +0200

    Változók létrehozása

--- FRONTEND/app.js
+++ FRONTEND/app.js
@@ -1,3 +1,11 @@
 document.getElementById('kalkulatorForm').addEventListener('submit', async (e) => {
     e.preventDefault();
+    const osszeg = document.getElementById('osszeg').value;
+    const kamat = document.getElementById('kamat').value;
+
+    const response = await fetch(`http://localhost:5000/api/ajanlatok?osszeg=${osszeg}&kamat=${kamat}`);
+    const data = await response.json();
+
+    const container = document.getElementById('ajanlatok');
+    container.innerHTML = '';
 });
