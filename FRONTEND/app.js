248de8297953ed62304a9f3c045163b9f10f72b4
Bernadetta Fabianová <florianfabian53@gmail.com>
2025-05-12 21:37:44 +0200

    Ikon hozzáadása a html head részéhez

--- FRONTEND/app.js
+++ FRONTEND/app.js
@@ -8,4 +8,23 @@ document.getElementById('kalkulatorForm').addEventListener('submit', async (e) =
 
     const container = document.getElementById('ajanlatok');
     container.innerHTML = '';
+    
+    data.forEach(ajanlat => {
+        let szin = 'bg-success text-white';
+        if (ajanlat.HaviTorleszto > 300000) szin = 'bg-danger text-white';
+        else if (ajanlat.HaviTorleszto > 150000) szin = 'bg-warning';
+
+        const card = `
+            <div class="col-md-4 mb-4">
+                <div class="card ${szin}">
+                    <div class="card-body">
+                        <h5 class="card-title">${ajanlat.Ev} év</h5>
+                        <p class="card-text">Havi törlesztés: ${ajanlat.HaviTorleszto.toLocaleString()} Ft</p>
+                        <p class="card-text">Összesen visszafizetendő: ${ajanlat.Visszafizetendo.toLocaleString()} Ft</p>
+                    </div>
+                </div>
+            </div>
+        `;
+        container.innerHTML += card;
+    });
 });
